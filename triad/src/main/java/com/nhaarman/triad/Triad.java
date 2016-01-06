5349bf5b978273d60e7e3a1741e0b0922e96ff65
Niek Haarman <haarman.niek@gmail.com>
2016-01-06 13:22:19 +0100

    Removed debug statement

--- triad/src/main/java/com/nhaarman/triad/Triad.java
+++ triad/src/main/java/com/nhaarman/triad/Triad.java
@@ -55,8 +55,6 @@ public class Triad {
     }
 
     private Triad(@NonNull final Backstack backstack, @Nullable final Listener<?> listener) {
-        new Exception().printStackTrace();
-
         mListener = listener;
         mBackstack = backstack;
 
